e3bd2215011a9d1dd32f743ab77cb4c3b90b5dcc
Marcos Viana <marcosvianacdc@gmail.com>
2023-10-11 13:29:50 -0300

    fix: Remove 'global' value.

--- packages/lib/src/setup.ts
+++ packages/lib/src/setup.ts
@@ -11,23 +11,26 @@ import { ApiHandler, QueueHandler, BusHandler } from "@/endpoints";
 export function eventHandlerSetup(options: EventHandlerSetupOptions) {
 	// Setup api event handler
 	const api = (...endpointArgs: EndpointArgs<ContextApi>) => {
-		const apiInstance = new ApiHandler(options, ...endpointArgs);
-		return async (...eventArgs: any[]) =>
-			await apiInstance.handleEvent(...eventArgs);
+		return async (...eventArgs: any[]) => {
+			const apiInstance = new ApiHandler(options, ...endpointArgs);
+			return await apiInstance.handleEvent(...eventArgs);
+		};
 	};
 
 	// Setup queue event handler
 	const queue = (...endpointArgs: EndpointArgs<ContextQueue>) => {
-		const queueInstance = new QueueHandler(options, ...endpointArgs);
-		return async (...eventArgs: any[]) =>
-			await queueInstance.handleEvent(...eventArgs);
+		return async (...eventArgs: any[]) => {
+			const queueInstance = new QueueHandler(options, ...endpointArgs);
+			return await queueInstance.handleEvent(...eventArgs);
+		};
 	};
 
 	// Setup bus event handler
 	const bus = (...endpointArgs: EndpointArgs<ContextBus>) => {
-		const busInstance = new BusHandler(options, ...endpointArgs);
-		return async (...eventArgs: any[]) =>
-			await busInstance.handleEvent(...eventArgs);
+		return async (...eventArgs: any[]) => {
+			const busInstance = new BusHandler(options, ...endpointArgs);
+			return await busInstance.handleEvent(...eventArgs);
+		};
 	};
 
 	return { api, queue, bus };
